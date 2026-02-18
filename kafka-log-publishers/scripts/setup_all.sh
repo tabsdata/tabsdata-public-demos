ad7810fbf548aab0a42bb808f9114401708a9e24
adaydan-tabsdata <daniel@tabsdata.com>
2026-02-17 17:30:12 -0800

    push

--- kafka-log-publishers/scripts/setup_all.sh
+++ kafka-log-publishers/scripts/setup_all.sh
@@ -3,6 +3,9 @@
 # Copyright 2026. Tabs Data Inc.
 #
 
+set -euo pipefail
+
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 source "${ROOT_DIR}/source.sh"
